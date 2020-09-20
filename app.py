fe39e400a554f9c755dd30e2841c9942f5b6c2f4
olliepage <products15816@gmail.com>
2020-09-20 14:07:41 +0100

    working properly. all matches from given page in single table with back and lay odds from given exchange.
    need to check a betfair example to see if that works. also need to add in the feature to plug in the various dates. shouldnt be too hard

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import oddsportal
+from bs4 import BeautifulSoup
+import pandas as pd
 
-html = oddsportal.fetch_html(url='https://www.oddsportal.com/soccer/england/premier-league-2019-2020/arsenal-watford-2JDks1o7/?r=1#cs;2;1')
+match_urls = oddsportal.return_soccer_url()
 
+for match_url in match_urls:
+    html = oddsportal.fetch_odds_html(url=match_url+'#cs;2;1',   # cs;2;1 will have the 0:0 odds enabled
+                                                  show_window=False,
+                                                  save=True)
+
+    soup = BeautifulSoup(html, "html.parser")
+    odds_store = oddsportal.get_odds_by_exchange(soup)
+
+    if 'reference_table' in locals():   # check if the variable exists in the local scope
+        additional_observation = pd.DataFrame.from_dict(odds_store).T
+        additional_observation['match_details'] = soup.title
+
+        reference_table = pd.concat([reference_table, additional_observation])
+
+    else:
+        reference_table = pd.DataFrame.from_dict(odds_store).T
+        reference_table['match_details'] = soup.title
+        reference_table.index.name = 'Exchanges'
--- oddsportal.py
+++ oddsportal.py
@@ -1,5 +1,11 @@
 from webbot import Browser
 from stringcase import snakecase
+from typing import List, NewType, Dict
+from bs4 import BeautifulSoup
+import re
+
+ResultSet = NewType('ResultSet', List)
+NavigableString = NewType('NavigableString', str)
 
 
 def login_oddsportal(url, show_window=True, save=False, chained_webbot=False):
@@ -22,8 +28,8 @@ def login_oddsportal(url, show_window=True, save=False, chained_webbot=False):
     driver.go_to(url)
     if chained_webbot:
         return driver
-    html = driver.get_page_source()
 
+    html = driver.get_page_source()
     page_title = snakecase(driver.get_title())
 
     driver.close_current_tab()
@@ -33,8 +39,30 @@ def login_oddsportal(url, show_window=True, save=False, chained_webbot=False):
     return html
 
 
-def fetch_odds(url, show_window=True, save=False):
+def return_soccer_url() -> List:
     """
+    fetch all the urls from the results page, and filter the results to just the urls that pertain to football matches
+
+    :return: (list)
+    """
+    html = login_oddsportal(url='https://www.oddsportal.com/soccer/england/premier-league/results/',
+                            show_window=False,
+                            save=True)
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    # fetch all the urls from the given page, then filter down to just the urls that pertain to football matches
+    raw_hrefs = soup.find_all(href=re.compile('/soccer/england/premier-league/[a-z]'))
+    raw_url = [ref['href'] for ref in raw_hrefs]
+    soccer_href_matches_url = [f'https://www.oddsportal.com/{s}' for s in raw_url if
+                               not any(xs in s for xs in ['results', 'standings', 'outrights'])]
+    return soccer_href_matches_url
+
+
+def fetch_odds_html(url, show_window=True, save=False):
+    """
+    returns the html of the given page on the 0:0 correct score odds
+
     :param url: the url to pass in to webbot to return to after logging in
     :param show_window: open and navigate through the GUI
     :param save: save the html in the session to file under 'test.html'
@@ -52,6 +80,56 @@ def fetch_odds(url, show_window=True, save=False):
             file.write(html)
 
     return html
+
+
+def split_exchanges_html(soup) -> Dict:
+    betting_exchanges_table = soup.find_all("table", class_="table-main detail-odds sortable")[1]
+    exchange_location: List[List] = [betting_exchanges_table.find_all(class_='name', href=re.compile(exchange)) for
+                                     exchange in ['matchbook', 'betfair']]
+    exchange_dict: Dict[NavigableString, List[ResultSet]] = {}
+    for i, v in enumerate(exchange_location):
+        try:
+            exchange_brand = v[0].string  # this evaluates to 'matchbook' or 'betfair'
+            exchange_dict[exchange_brand] = v
+        except IndexError:
+            print('no more exchanges present in list')
+            continue
+    return exchange_dict
+
+# place the html for the matchbook row and the betfair row in separate elements in a list
+
+
+def get_odds_by_exchange(soup) -> Dict[str, Dict[str, str]]:
+    """
+    Iterates through the split_exchanges_html dict and extracts the 'Back' and 'Lay' odds for every exchange present
+    and returns it in a dictionary
+
+    :param soup: BeautifulSoup object that has parsed the oddsportal html
+    :return: (dict) Booking Exchanges and their respective Back & Lay odds
+    """
+    exchange_dict = split_exchanges_html(soup)
+    odds_store: Dict[str, Dict[str, str]] = {}
+    for k, v in exchange_dict.items():
+        odds_store[str(k)] = {}
+        parent_row = k.parent.parent.parent.parent.parent
+
+        for cell in parent_row.find_all("td", class_=re.compile("(center|right odds)")):
+            if 'colspan' in cell.attrs:
+                break
+            if not cell.string:
+                if 'Back' in odds_store[str(k)] and odds_store[str(k)]['Back'] is None:
+                    odds_store[str(k)]['Back'] = cell.div.contents[0]
+                elif 'Lay' in odds_store[str(k)] and odds_store[str(k)]['Lay'] is None:
+                    odds_store[str(k)]['Lay'] = cell.div.contents[0]
+                else:
+                    print('> Unhandled output:')
+                    print(cell.div.contents)
+                    print('*************', end='\n\n')
+                continue
+            if cell.string == 'Back' or cell.string == 'Lay':
+                odds_store[str(k)][cell.string] = None
+
+    return odds_store
 
 
 
